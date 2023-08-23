ef061421dd8839090432f65330ab521c20684bd3
mbachibueze <mbachibueze27@gmail.com>
2023-08-23 10:17:06 +0100

    Api added

---
+++ script.js
@@ -0,0 +1,36 @@
+// script.js
+
+const accessKey = 'YOUR_UNSPLASH_ACCESS_KEY'; // Replace with your actual Unsplash access key
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}`;
+
+async function fetchImages() {
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    displayImages(data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+function displayImages(images) {
+  const imageContainer = document.getElementById('image-container');
+
+  // Clear previous content if any
+  imageContainer.innerHTML = '';
+
+  // Display fetched images
+  images.forEach(image => {
+    const imageElement = document.createElement('img');
+    imageElement.src = image.urls.regular;
+    imageElement.alt = image.alt_description;
+    imageContainer.appendChild(imageElement);
+  });
+}
+
+fetchImages();
